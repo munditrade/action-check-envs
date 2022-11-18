fdd85630d8d3681d0a762fa9d09e4e14a7888730
Mario Matias Magallanes <matias.magallanes@mundi.io>
2022-11-18 11:31:41 -0300

    General Refactor

--- .github/scripts/check.py
+++ .github/scripts/check.py
@@ -6,45 +6,87 @@ import sys
 import hvac
 import yaml
 
+
+def promote():
+    '''
+    Check for promote workflow
+    '''
+    service = sys.argv[2]
+    environment = sys.argv[3]
+    all_secrets = client.secrets.kv.v1.read_secret(
+        path=service + '/' + environment, mount_point=rootEngine)
+    file = f'deploy/{environment}/{service}.yaml'
+    missing_secrets = []
+    with open(file, "r", encoding="utf-8") as stream:
+        try:
+            for env in yaml.safe_load(stream)['env']:
+                if 'secret' in env.keys():
+                    if env['secret'] in all_secrets['data'].keys():
+                        pass
+                    else:
+                        missing_secrets.append(
+                            f"{rootEngine}/{service}/{environment}/{env['secret']}")
+        except yaml.YAMLError as exc:
+            print(exc)
+    if len(missing_secrets) > 0:
+        print("::"*40)
+        print(':: There are differences in Environment Variables and Vault Secrets')
+        print(':: Missing Secrets')
+        for item in missing_secrets:
+            print(item)
+        print("::"*40)
+        sys.exit(1)
+
+def envs():
+    '''
+    Check for env workflow
+    '''
+    changed_files = []
+    for file_name in sys.argv[2:]:
+        if file_name.startswith("deploy/"):
+            changed_files.append(file_name)
+
+    if client.is_authenticated():
+        missing_secrets = []
+        for file in changed_files:
+            splitted = file.split('/')
+            environment = splitted[1]
+            service = splitted[2].split('.')[0]
+            all_secrets = client.secrets.kv.v1.read_secret(
+                path=service + '/' + environment, mount_point=rootEngine)
+            with open(file, "r", encoding="utf-8") as stream:
+                try:
+                    for env in yaml.safe_load(stream)['env']:
+                        if 'secret' in env.keys():
+                            if env['secret'] in all_secrets['data'].keys():
+                                pass
+                            else:
+                                missing_secrets.append(
+                                    f"{rootEngine}/{service}/{environment}/{env['secret']}")
+                except yaml.YAMLError as exc:
+                    print(exc)
+        if len(missing_secrets) > 0:
+            print("::"*40)
+            print(':: There are differences in Environment Variables and Vault Secrets')
+            print(':: Missing Secrets')
+            for item in missing_secrets:
+                print(item)
+            print("::"*40)
+            sys.exit(1)
+    else:
+        print("::"*40)
+        print(":: Error in Vault authentication")
+        print("::"*40)
+        sys.exit(1)
+
+
 vault_url = os.environ['ADDR']
 client = hvac.Client(url=vault_url)
 client.token = os.environ['TOKEN']
 rootEngine = os.environ['ROOT_ENGINE']
-
-changedFiles = []
-for fileName in sys.argv[1:]:
-    if fileName.startswith("deploy/"):
-        changedFiles.append(fileName)
-
-if client.is_authenticated():
-    missingSecrets = []
-    for file in changedFiles:
-        splitted = file.split('/')
-        environment = splitted[1]
-        service = splitted[2].split('.')[0]
-        all_secrets = client.secrets.kv.v1.read_secret(
-            path=service + '/' + environment, mount_point=rootEngine)
-        with open(file, "r", encoding="utf-8") as stream:
-            try:
-                for env in yaml.safe_load(stream)['env']:
-                    if 'secret' in env.keys():
-                        if env['secret'] in all_secrets['data'].keys():
-                            pass
-                        else:
-                            missingSecrets.append(
-                                f"{rootEngine}/{service}/{environment}/{env['secret']}")
-            except yaml.YAMLError as exc:
-                print(exc)
-    if len(missingSecrets) > 0:
-        print("::"*40)
-        print(':: There are differences in Environment Variables and Vault Secrets')
-        print(':: Missing Secrets')
-        for item in missingSecrets:
-            print(item)
-        print("::"*40)
-        sys.exit(1)
+if sys.argv[1] == 'env':
+    envs()
+elif sys.argv[1] == 'promote':
+    promote()
 else:
-    print("::"*40)
-    print(":: Error in Vault authentication")
-    print("::"*40)
-    sys.exit(1)
+    print("::Wrong Workflow Name (not \'env\' neither \'promote\')")
--- README.md
+++ README.md
@@ -4,19 +4,17 @@ This Github action perform a validation of environment variables in deployment
 
 ## Parameters
 
-| Name        | Description                            | Required |
-|-------------|----------------------------------------|----------|
-|    addr     | ADDR, e.g: https://my.example.com:2345 |   True   |
-|    token    | TOKEN to use in ADDR                   |   True   |
-| root_engine | Root Engine Path                       |   True   |
+| Name         | Description                            | Required |
+|--------------|----------------------------------------|----------|
+|    addr      | ADDR, e.g: https://my.example.com:2345 |   True   |
+|    token     | TOKEN to use in ADDR                   |   True   |
+| root_engine  | Root Engine Path                       |   True   |
+| service_name | Service Name.                          |   False  |
 
-## Dependencies
-
-munditrade/get-changed-files
 
 ## Usage
 
-- Usage in your workflow is like following example:
+- Usage in your workflow is like following examples:
 
 ```yaml
 name: MyWorkflow
@@ -36,5 +34,29 @@ jobs:
           addr: ${{ secrets.ADDR }}
           token: ${{ secrets.TOKEN }}
           root_engine: ${{ secrets.ROOT_ENGINE }}
+
+```
+
+Or with service_name
+
+```yaml
+name: MyWorkflow
+
+on:
+  push:
+
+jobs:
+  myexamplejob:
+    runs-on: ubuntu-latest
+    name: dosomething
+    steps:
+      - name: Check Envs
+        uses: munditrade/action-check-envs@main
+        id: check
+        with:
+          addr: ${{ secrets.ADDR }}
+          token: ${{ secrets.TOKEN }}
+          root_engine: ${{ secrets.ROOT_ENGINE }}
+          service_name: ${{ secrets.SERVICE_NAME }}
 
 ```
